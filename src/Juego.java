d59010006647dbce9464b754efb479e2d7126ca4
juanpablosb04 <juanpablosb04@hotmail.com>
2023-08-01 20:32:56 -0600

    Se agrega el sistema de ordenes - Genera orden inicial - Ordenes cada 20 seg - Ordenes en orden - Si hay ordenes no genera mas hasta que haya espacio

--- src/Juego.java
+++ src/Juego.java
@@ -1,8 +1,11 @@
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.util.ArrayList;
+import java.util.Random;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -14,7 +17,11 @@ import javax.swing.JPanel;
  * @author Juan Pablo
  */
 public class Juego extends javax.swing.JFrame {
-
+    ArrayList al = new ArrayList(3);
+    Timer tiempoOrden1;
+    Timer tiempoOrden2;
+    Timer tiempoOrden3;
+    private int pts = 0;
     /**
      * Creates new form Juego
      */
@@ -29,7 +36,31 @@ public class Juego extends javax.swing.JFrame {
         BotonD.setOpaque(false);
         BotonD.setContentAreaFilled(false);
         BotonD.setBorderPainted(false);
+        Random r = new Random();
         
+        String elementoAzar;
+        al.add("HAMBURGUESA DE CARNE: Pan,Carne");
+        al.add("HAMBURGUESA DE QUESO: Pan, Carne y Queso");
+        al.add("HAMBURGUESA CLASICA: Pan, Carne, Queso y Lechuga");
+        
+        int a = (int) (Math.random()* 3);
+        String txt = (String) al.get(a);
+        ordenPrincipal1.setText(txt);
+        
+        tiempoOrden2 = new Timer(5000, null);
+        tiempoOrden2.start();
+        tiempoOrden2.addActionListener(new java.awt.event.ActionListener(){
+        @Override
+        public void actionPerformed(java.awt.event.ActionEvent evt){
+            if(ordenSiguiente.getText().equals("Esperando orden...")){
+                generarSiguienteOrden();
+            }else if(ordenLejana.getText().equals("Esperando orden...")){
+                generarOrdenLejana();
+            }else{
+            
+            }
+           }
+        });
     }
 
     /**
@@ -41,23 +72,40 @@ public class Juego extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        ordenPrincipal = new javax.swing.JLabel();
+        orden2 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
         jPanel1 = new FondoPartida();
         BotonI = new javax.swing.JButton();
         BotonD = new javax.swing.JButton();
         PanelPedido = new javax.swing.JPanel();
+        lbl3 = new javax.swing.JLabel();
+        ordenPrincipal1 = new javax.swing.JLabel();
+        lbl1 = new javax.swing.JLabel();
+        ordenSiguiente = new javax.swing.JLabel();
+        lbl2 = new javax.swing.JLabel();
+        ordenLejana = new javax.swing.JLabel();
+
+        ordenPrincipal.setFont(new java.awt.Font("Arial Black", 0, 14)); // NOI18N
+        ordenPrincipal.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ordenPrincipal.setText("Esperando orden...");
+
+        orden2.setText("Esperando orden...");
+
+        jLabel2.setText("Orden mas Lejana");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        jPanel1.add(BotonI, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 350, 130, 60));
+        jPanel1.add(BotonI, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 460, 130, 60));
 
         BotonD.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BotonDActionPerformed(evt);
             }
         });
-        jPanel1.add(BotonD, new org.netbeans.lib.awtextra.AbsoluteConstraints(860, 390, 130, 60));
+        jPanel1.add(BotonD, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 460, 130, 60));
 
         PanelPedido.setBackground(new java.awt.Color(204, 204, 204));
 
@@ -74,7 +122,34 @@ public class Juego extends javax.swing.JFrame {
 
         jPanel1.add(PanelPedido, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 6, -1, -1));
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(2, 0, 1000, 530));
+        lbl3.setFont(new java.awt.Font("Elephant", 1, 24)); // NOI18N
+        lbl3.setForeground(new java.awt.Color(255, 255, 255));
+        lbl3.setText("ORDEN ACTIVA");
+        jPanel1.add(lbl3, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 90, -1, -1));
+
+        ordenPrincipal1.setFont(new java.awt.Font("Arial Black", 0, 14)); // NOI18N
+        ordenPrincipal1.setForeground(new java.awt.Color(255, 255, 255));
+        ordenPrincipal1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ordenPrincipal1.setText("Esperando orden...");
+        jPanel1.add(ordenPrincipal1, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 120, 370, 20));
+
+        lbl1.setForeground(new java.awt.Color(255, 255, 255));
+        lbl1.setText("Siguiente Orden");
+        jPanel1.add(lbl1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, -1, 20));
+
+        ordenSiguiente.setForeground(new java.awt.Color(255, 255, 255));
+        ordenSiguiente.setText("Esperando orden...");
+        jPanel1.add(ordenSiguiente, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 10, 280, -1));
+
+        lbl2.setForeground(new java.awt.Color(255, 255, 255));
+        lbl2.setText("Orden mas Lejana");
+        jPanel1.add(lbl2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 40, -1, -1));
+
+        ordenLejana.setForeground(new java.awt.Color(255, 255, 255));
+        ordenLejana.setText("Esperando orden...");
+        jPanel1.add(ordenLejana, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 40, -1, -1));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1000, 530));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -122,15 +197,34 @@ public class Juego extends javax.swing.JFrame {
     private javax.swing.JButton BotonD;
     private javax.swing.JButton BotonI;
     public javax.swing.JPanel PanelPedido;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lbl1;
+    private javax.swing.JLabel lbl2;
+    private javax.swing.JLabel lbl3;
+    private javax.swing.JLabel orden2;
+    private javax.swing.JLabel ordenLejana;
+    private javax.swing.JLabel ordenPrincipal;
+    private javax.swing.JLabel ordenPrincipal1;
+    private javax.swing.JLabel ordenSiguiente;
     // End of variables declaration//GEN-END:variables
     class FondoPartida extends JPanel{
         private Image imagen;
         public void paint (Graphics g){
-            imagen = new ImageIcon(getClass().getResource("Imagenes/Fondo.jpg")).getImage();
+            imagen = new ImageIcon(getClass().getResource("Imagenes/fondonuevo.jpg")).getImage();
             g.drawImage(imagen, 0, 0, getWidth(),getHeight(), this);
             setOpaque(false);
             super.paint(g);
         }
     }
+    public void generarSiguienteOrden(){
+    int a = (int) (Math.random()* 3);
+    String txt1 = (String) al.get(a);
+    ordenSiguiente.setText(txt1);
+}
+public void generarOrdenLejana(){
+    int a = (int) (Math.random()* 3);
+    String txt2 = (String) al.get(a);
+    ordenLejana.setText(txt2);
+}
 }
